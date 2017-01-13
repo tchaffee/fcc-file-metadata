d14f64e143397fd89a9d87890a469360d001de93
Todd Chaffee <tc@toddbiz.com>
2017-01-13 09:06:25 +0000

    First working version.

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
+app.use(express.static('public'));
+
+app.post('/get-file-size', upload.single('uploadedFile'), function (req, res, next) {
+  res.send({
+    size: req.file.size
+  });
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080.');
+  console.log('App listening on port 8080.');
 });
